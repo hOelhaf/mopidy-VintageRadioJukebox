f2d25db6e95e3d8dc576d200b8c5a840bea372d8
Holger Oelhaf <h.oelhaf@googlemail.com>
2020-10-10 17:18:48 +0200

    Basic jukebox implementation

--- mopidy_vintageradiojukebox/frontend.py
+++ mopidy_vintageradiojukebox/frontend.py
@@ -1,12 +1,13 @@
 import logging
 import traceback
+import os
+import pykka
+import re
 
 from mopidy import core
 
-import pykka
-
-from .main_menu import MainMenu
 from .tts import TTS
+from .gpio_input_manager import GPIOButton, GPIORotaryEncoder
 
 logger = logging.getLogger(__name__)
 
@@ -15,104 +16,79 @@ class VintageRadioJukebox(pykka.ThreadingActor, core.CoreListener):
 
     def __init__(self, config, core):
         super(VintageRadioJukebox, self).__init__()
-        self.tts = TTS()
-        self.menu = False
+        #self.tts = TTS()
         self.core = core
-        self.main_menu = MainMenu(self)
-
-        self.debug_gpio_simulate = config['VintageRadioJukebox']['debug_gpio_simulate']
-        if self.debug_gpio_simulate:
-            from .gpio_simulator import GpioSimulator
-            self.simulator = GpioSimulator(self)
-        else:
-            from .gpio_input_manager import GPIOManager
-            self.gpio_manager = GPIOManager(self, config['ttsgpio'])
         
-        self.playlistMapping = {}
-        for k, v in config['VintageRadioJukebox'].items():
-            if 'playlist' in k:
-                s = v.split(',')
-                i = int(s[0])
-                playlistMapping[i] = s[1].strip()
-        
-        self.shutdownPin = config['VintageRadioJukebox']['shutdown']
-        self.navPins = (config['VintageRadioJukebox']['nav_EncA'], config['VintageRadioJukebox']['nav_EncB'])
+        config_gpios = dict((k, v) for (k, v) in config['VintageRadioJukebox'].items() if 'bcm' in k.lower())
+        # get playlist mapping from config
+        self.gpio_playlists = []
+        config_playlists = dict((k, v) for (k, v) in config_gpios.items() if 'playlist' in v.lower())
+        for k, v in config_playlists.items():
+            gpio_pin = int(k.replace("bcm", ""))
+            name = v.replace("playlist", "").strip()
 
-    def track_playback_started(self, tl_track):
-        self.speak_current_song(tl_track)
-
-    def playback_state_changed(self, old_state, new_state):
-        if self.debug_gpio_simulate:
-            if new_state == core.PlaybackState.PLAYING:
-                self.simulator.playing_led.select()
+            playlist = self._get_playlist_by_name(name)
+            if playlist:
+                logger.info("Register playlist " + playlist.name + " at GPIO pin " + str(gpio_pin))
+                self.gpio_playlists.append(GPIOButton(self, gpio_pin, self.input_playlist, playlist))
             else:
-                self.simulator.playing_led.deselect()
-        else:
-            if new_state == core.PlaybackState.PLAYING:
-                self.gpio_manager.set_led(True)
-            else:
-                self.gpio_manager.set_led(False)
+                logger.info("Playlist " + name + " not found.")
 
-    def input(self, input_event):
-        try:
-            if input_event['key'] == 'volume_up':
-                if input_event['long']:
-                    self.repeat()
-                else:
-                    current = self.core.playback.volume.get()
-                    current += 10
-                    self.core.playback.volume = current
-            elif input_event['key'] == 'volume_down':
-                if input_event['long']:
-                    current = 0
-                else:
-                    current = self.core.playback.volume.get()
-                    current -= 10
-                self.core.playback.volume = current
-            elif input_event['key'] == 'main' and input_event['long'] \
-                    and self.menu:
-                self.exit_menu()
-            else:
-                if self.menu:
-                    self.main_menu.input(input_event)
-                else:
-                    self.manage_input(input_event)
+        # get generic button mapping from config
+        self.gpio_buttons = []
+        config_shutdown = [(k, v) for (k, v) in config_gpios.items() if 'shutdown' in v.lower()]
+        if config_shutdown:
+            gpio_pin = int(config_shutdown[0][0].replace("bcm", ""))
+            logger.info("Register button shutdown at GPIO pin " + str(gpio_pin))                        
+            self.gpio_buttons.append(GPIOButton(self, gpio_pin, self.input_shutdown, 'shutdown')) 
 
-        except Exception:
-            traceback.print_exc()
+        # get rotary encoder mapping from config
+        self.gpio_rotaryEncs = []
+        config_rotary_encoders = dict((k, v) for (k, v) in config_gpios.items() if 'rotary_encoder' in v.lower())
+        rotary_encoder_names = {re.sub(r'rotary_encoder_[ABab]', "", v).strip() for (k, v) in config_rotary_encoders.items()}
+        for name in rotary_encoder_names:
+            r_enc = dict((k, v) for (k, v) in config_rotary_encoders.items() if name in v)
+            config_rotary_A = [k for (k, v) in r_enc.items() if 'rotary_encoder_a' in v.lower()]
+            config_rotary_B = [k for (k, v) in r_enc.items() if 'rotary_encoder_b' in v.lower()]
+            if config_rotary_A and config_rotary_B:
+                gpio_A = int(config_rotary_A[0].replace("bcm", ""))
+                gpio_B = int(config_rotary_B[0].replace("bcm", ""))
+                logger.info("Register rotary encoder " + name + " at GPIO pins " + str(gpio_A) + ", " + str(gpio_B))
+                self.gpio_rotaryEncs.append(GPIORotaryEncoder(self, gpio_A, gpio_B, self.input_rotary_encoder, name))
 
-    def manage_input(self, input_event):
-        if input_event['key'] == 'next':
+    def on_start(self):
+        # read all playlist gpio's to select a playlist 
+        for gpio_playlist in self.gpio_playlists:
+            gpio_playlist.event_handler(gpio_playlist.channel)
+
+    def on_stop(self):
+        pass
+    
+    def input_playlist(self, playlist):
+        logger.info("Playlist " + playlist.name + " event received")
+        self.core.tracklist.clear()
+        tracks = self.core.playlists.get_items(playlist.uri).get()
+        logger.info(tracks)
+        track_uris = [track.uri for track in tracks]
+        logger.info("Tracks: {0}".format(track_uris))
+        self.core.tracklist.add(uris=track_uris)
+        self.core.playback.play()
+
+    def input_shutdown(self, name):
+        logger.debug(name + " event received")
+        if name == 'shutdown':
+            #self.tts.speak_text(name)
+            os.system("shutdown now -h")
+
+    def input_rotary_encoder(self, name, value):
+        logger.info(name + " event received. value="+str(value))
+        if value > 0:
             self.core.playback.next()
-        elif input_event['key'] == 'previous':
-            self.core.playback.previous()
-        elif input_event['key'] == 'main':
-            if input_event['long']:
-                self.menu = True
-                self.main_menu.reset()
-            else:
-                if self.core.playback.state.get() == \
-                        core.PlaybackState.PLAYING:
-                    self.core.playback.pause()
-                else:
-                    self.core.playback.play()
-
-    def repeat(self):
-        if self.menu:
-            self.main_menu.repeat()
         else:
-            self.speak_current_song(self.core.playback.current_tl_track.get())
+            self.core.playback.previous()
 
-    def speak_current_song(self, tl_track):
-        if tl_track is not None:
-            artists = ""
-            for artist in tl_track.track.artists:
-                artists += artist.name + ","
-            self.tts.speak_text(tl_track.track.name + ' by ' + artists)
-
-    def exit_menu(self):
-        self.menu = False
-
-    def playlists_loaded(self):
-        self.main_menu.elements[0].reload_playlists()
-        self.tts.speak_text("Playlists loaded")
+    def _get_playlist_by_name(self, name):
+        for playlist in self.core.playlists.as_list().get():
+            if name.lower() == playlist.name.lower():
+                return playlist
+        return
